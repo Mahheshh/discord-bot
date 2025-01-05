b918291ada809cc079222465b2584d37076dc934
Mahheshh <140879497+Mahheshh@users.noreply.github.com>
2025-01-05 17:03:37 +0530

    final

--- src/app/interactions/route.ts
+++ src/app/interactions/route.ts
@@ -2,10 +2,44 @@ import { NextRequest } from "next/server";
 
 import { Interaction, InteractionResponse } from "@/types/interaction";
 import { InteractionResponseType, InteractionType } from "discord-interactions";
-import initializeAgent from "@/lib/initAgent";
 import { HumanMessage } from "@langchain/core/messages";
 
 import { verifyInteractionRequest } from "@/discord/verifyReq";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { createSolanaTools, SolanaAgentKit } from "solana-agent-kit";
+import { MemorySaver } from "@langchain/langgraph";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+
+const llm = new ChatGoogleGenerativeAI({
+  modelName: "gemini-pro",
+  temperature: 0.7,
+});
+
+const solanaAgent = new SolanaAgentKit(
+  process.env.SOLANA_PRIVATE_KEY!,
+  process.env.RPC_URL!,
+  {
+    OPENAI_API_KEY: process.env.GOOGLE_APPLICATION_CREDENTIALS!,
+  }
+);
+
+const tools = createSolanaTools(solanaAgent);
+const memory = new MemorySaver();
+
+const agent = createReactAgent({
+  llm,
+  tools,
+  checkpointSaver: memory,
+  messageModifier: `
+          You are a helpful agent that can interact onchain using the Solana Agent Kit. You are
+          empowered to interact onchain using your tools. If you ever need funds, you can request them from the
+          faucet. If not, you can provide your wallet details and request funds from the user. If there is a 5XX
+          (internal) HTTP error code, ask the user to try again later. If someone asks you to do something you
+          can't do with your currently available tools, you must say so, and encourage them to implement it
+          themselves using the Solana Agent Kit, recommend they go to https://www.solanaagentkit.xyz for more information. Be
+          concise and helpful with your responses. Refrain from restating your tools' descriptions unless it is explicitly requested.
+        `,
+});
 
 export async function POST(req: NextRequest) {
   console.log("Received POST request");
@@ -76,20 +110,26 @@ export async function POST(req: NextRequest) {
   const message = interaction.data.options[0].value;
   console.log("Processing message:", message);
 
-  const { agent, config } = await initializeAgent();
-  console.log("Agent initialized");
-
   const stream = await agent.stream(
     { messages: [new HumanMessage(message)] },
-    config
+    {
+      configurable: {
+        thread_id: interaction.user.id || "solana agent kit",
+      },
+    }
   );
 
   let botResponse = "";
   try {
     for await (const chunk of stream) {
-      if ("agent" in chunk) {
-        botResponse += chunk.agent.messages[0].content;
+      if (!("agent" in chunk) || !chunk.agent?.messages?.[0]?.content) {
+        continue;
       }
+      if (typeof chunk.agent.messages[0].content !== "string") {
+        continue;
+      }
+      botResponse += chunk.agent.messages[0].content;
+      console.log("chunk: ", chunk.agent.messages[0].content);
     }
     console.log("Bot response:", botResponse);
     return Response.json(
--- src/types/interaction.ts
+++ src/types/interaction.ts
@@ -1,28 +1,31 @@
-import { InteractionResponseType } from "discord-interactions"
+import { InteractionResponseType } from "discord-interactions";
 
 type InteractionDataOption = {
-    name: string, 
-    type: number, 
-    value: string // contains passed in message, text parameter
-}
+  name: string;
+  type: number;
+  value: string; // contains passed in message, text parameter
+};
 
 type InteractionData = {
-    id: string, 
-    name: string, // command name
-    options: Array<InteractionDataOption>
-}
+  id: string;
+  name: string; // command name
+  options: Array<InteractionDataOption>;
+};
 
 type Interaction = {
-    data: InteractionData,
-    token: string,
-    type: number
-}
+  data: InteractionData;
+  user: {
+    id: string;
+  };
+  token: string;
+  type: number;
+};
 
 type InteractionResponse = {
-    type: InteractionResponseType,
-    data: {
-        content?: string
-    }
-}
+  type: InteractionResponseType;
+  data: {
+    content?: string;
+  };
+};
 
 export type { Interaction, InteractionResponse };
